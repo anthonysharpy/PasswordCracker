14d8cdeef267e017e604586126f69b7bc45cb883
Anthony Sharp <anthonysharp15@gmail.com>
2020-12-19 21:45:48 +0000

    getting there

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Security.Cryptography;
+using System.Diagnostics;
 
 namespace PaswordCracker
 {
@@ -22,27 +23,77 @@ namespace PaswordCracker
     public partial class MainWindow : Window
     {
         string password = "";
+        string passwordhash = "";
+        ulong possibilities = 0;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        string computeHash(string input)
+        static string computeHash(string input)
         {
             SHA256 hash = SHA256.Create();
-            hash.ComputeHash(Encoding.ASCII.GetBytes(password));
-            return Encoding.ASCII.GetString(hash.Hash);
+            hash.ComputeHash(Encoding.ASCII.GetBytes(input));
+
+            string output = "";
+
+            for(int i = 0; i < 32; i++) // sha256 returns 32 bytes
+            {
+                output += Convert.ToInt32(hash.Hash[i]);
+            }
+
+            return output;
         }
 
-        private void crack_button_MouseUp(object sender, MouseButtonEventArgs e)
+        ulong howManyPossiblePasswords(int passwordlength)
+        {
+            return 36 ^ (ulong)passwordlength;
+        }
+
+        private void crackButtonClick(object sender, RoutedEventArgs e)
         {
             password = ((TextBox)FindName("password_box")).Text;
+            passwordhash = computeHash(password);
 
-            ((TextBlock)FindName("hash_box")).Text = "Your SHA-1 hash: " + computeHash(password);
+            ((TextBlock)FindName("hash_box")).Text = "Your SHA-1 hash: " + passwordhash;
+
+            doCrack();
         }
 
-        private void stop_button_MouseUp(object sender, MouseButtonEventArgs e)
+        string incrementString(string currentstring)
+        {
+            if(currentstring.Substring(currentstring.Length-1, 1) != "z")
+            {
+                currentstring.
+            }
+
+            return currentstring;
+        }
+
+        void doCrack()
+        {
+            possibilities = howManyPossiblePasswords(password.Length);
+
+            string curstring = "a";
+
+            TextBlock outputbox = (TextBlock)FindName("output_text");
+
+            for(ulong i = 0; i < possibilities; i++)
+            {
+                outputbox.Text += "Trying " + curstring + "\n";
+
+                if (computeHash(curstring) == passwordhash)
+                {
+                    outputbox.Text += "Found password: " + curstring + "\n";
+                    return;
+                }
+
+                curstring = incrementString(curstring);
+            }
+        }
+
+        private void stopButtonClick(object sender, RoutedEventArgs e)
         {
 
         }
